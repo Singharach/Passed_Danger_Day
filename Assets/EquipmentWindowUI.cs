9b698bbeebc3ef92dd327b28aba8ed5e5494dd1a
Sangarun Phanchan <sangarun2544@gmail.com>
2023-01-25 19:35:21 +0700

    Equipment Screen

---
+++ Assets/EquipmentWindowUI.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SG
+{
+public class EquipmentWindowUI : MonoBehaviour
+{
+    public bool rightHandSlot01Selected;
+    public bool rightHandSlot02Selected;
+    public bool leftHandSlot01Selected;
+    public bool leftHandSlot02Selected;
+
+    HandEquipmentSlotUI[] handEquipmentSlotUI;
+
+    private void Start()
+    {
+        handEquipmentSlotUI = GetComponentsInChildren<HandEquipmentSlotUI>();
+    }
+
+    public void LoadWeaponsOnEquipmentScreen(PlayerInventory playerInventory)
+    {
+        for (int i = 0; 1 < handEquipmentSlotUI.Length; i++)
+        {
+            if (handEquipmentSlotUI[i].rightHandSlot01)
+            {
+                handEquipmentSlotUI[i].AddItem(playerInventory.weaponInRightHandSlots[0]);
+            }
+            else if (handEquipmentSlotUI[i].rightHandSlot02)
+            {
+                handEquipmentSlotUI[i].AddItem(playerInventory.weaponInRightHandSlots[1]);
+            }
+            else if (handEquipmentSlotUI[i].leftHandSlot01)
+            {
+                handEquipmentSlotUI[i].AddItem(playerInventory.weaponInLeftHandSlots[0]);
+            }
+            else
+            {
+                handEquipmentSlotUI[i].AddItem(playerInventory.weaponInLeftHandSlots[1]);
+            }
+        }
+    }
+
+    public void SelectRightHandSlot01()
+    {
+        rightHandSlot01Selected = true;
+    }
+
+    public void SelectRightHandSlot02()
+    {
+        rightHandSlot01Selected = true;
+    }
+
+    public void SelectLeftHandSlot01()
+    {
+        leftHandSlot01Selected = true;
+    }
+
+    public void SelectLeftHandSlot02()
+    {
+        leftHandSlot02Selected = true;
+    }
+}
+}
--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -8,6 +8,7 @@ namespace SG
 public class UIManager : MonoBehaviour
 {
     public PlayerInventory playerInventory;
+    EquipmentWindowUI equipmentWindowUI;
 
     [Header("UI Window")]
     public GameObject hudWindow;
@@ -19,9 +20,15 @@ public class UIManager : MonoBehaviour
     public Transform weaponInventorySlotsParent;
     WeaponInventorySlot[] weaponInventorySlots;
 
+    private void Awake()
+    {
+        equipmentWindowUI = FindObjectOfType<EquipmentWindowUI>();
+    }
+
     private void Start()
     {
         weaponInventorySlots = weaponInventorySlotsParent.GetComponentsInChildren<WeaponInventorySlot>();
+        equipmentWindowUI.LoadWeaponsOnEquipmentScreen(playerInventory);
     }
 
     public void UpdateUI()
